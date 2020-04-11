8db4a76d40c69ee34a7e592345e49407e4a3b769
NotJishy <jishydoods@gmail.com>
2020-04-11 19:54:37 -0400

    History and President commands now track how many times each response has been chosen.
    Presidents are now in a assets/presidents/presidents.json

--- commands/history.js
+++ commands/history.js
@@ -1,7 +1,12 @@
 exports.history = function (msg, Discord) {
+
+    var fs = require('fs')
+
     let facts = require('../assets/history/facts.json')
 
     let selectFact = facts[Math.floor(Math.random() * facts.length)]
+
+    selectFact.count += 1;
 
     const attachment = new Discord.Attachment(`./assets/history/${selectFact.name}.jpg`, 'history.jpg')
 
@@ -9,5 +14,12 @@ exports.history = function (msg, Discord) {
         .setDescription(selectFact.desc)
         .attachFile(attachment)
         .setImage('attachment://history.jpg')
+        .setFooter(`This historical fact has been chosen ${selectFact.count} times.`)
     msg.channel.send(embed)
+
+    fs.writeFile('./assets/history/facts.json', JSON.stringify(facts), (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
 }
