d4b7ca8642bbc8576e9c2f6bb7f2de1ce993b029
NotJishy <jishydoods@gmail.com>
2020-04-11 15:41:26 -0400

    Rewrote msgDelete.js
    Messages without text (i.e. embeds and images) no longer give an error when deleted

--- listeners/msgDelete.js
+++ listeners/msgDelete.js
@@ -1,56 +1,23 @@
-exports.msgDelete = async (bot, messageDelete, logCH, darkblue, date, time, timeLogs) => {
+exports.msgDelete = async (bot, messageDelete, logCH, darkblue, date, time, timeLogs, Discord) => {
 
-    var fs = require("fs");
+    var body = ""
+    if (!messageDelete.content) {
 
-    const dir = ('./logs/deletedmessages/' + (`${messageDelete.author.id}`) + '/' + (date))
+        body = "`null`"
 
-    bot.channels.get(logCH).send({
-        embed: {
-            color: darkblue,
-            author: {
-                name: (`${messageDelete.author.username}`),
-                icon_url: (`${messageDelete.author.avatarURL}`)
-            },
-            title: "Message Deleted:",
-            description: "Message sent by " + (`${messageDelete.author}`) + " has been deleted.",
-            fields: [{
-                    name: "Message content:",
-                    value: (`${messageDelete.content}`)
-                },
-                {
-                    name: "Channel",
-                    value: "#" + (`${messageDelete.channel.name}`)
-                },
-                {
-                    name: "Date & Time:",
-                    value: (date) + " @ " + (time)
-                }
-            ]
-        }
-    });
-    if (!fs.existsSync('./logs/deletedmessages/' + (`${messageDelete.author.id}`))) {
-        fs.mkdirSync('./logs/deletedmessages/' + (`${messageDelete.author.id}`));
-    };
-    if (!fs.existsSync('./logs/deletedmessages/' + (`${messageDelete.author.id}`) + '/' + (date))) {
-        fs.mkdirSync('./logs/deletedmessages/' + (`${messageDelete.author.id}`) + '/' + (date));
-    };
-    fs.readdir(dir, function (err, files) {
-        var totalFiles = files.length;
+    } else {
 
-        if (!fs.existsSync('./logs/deletedmessages/' + (`${messageDelete.author.id}`) + '/' + (date) + '/' + (timeLogs) + '.txt')) {
-            fs.writeFile('./logs/deletedmessages/' + (`${messageDelete.author.id}`) + '/' + (date) + '/' + (timeLogs) + '.txt', 'Message from ' + (`${messageDelete.author}`) + '(' + (`${messageDelete.author.username}`) + ') has been deleted.\n\nMessage Content:\n' + (`${messageDelete.content}`) + '\n\nChannel:\n#' + (`${messageDelete.channel.name}`) + '\n\nDate & Time:\n' + (date) + ' @ ' + (time), function (err) {
-                if (err) {
-                    return console.log(err)
-                    .then(bot.channels.get(logCH).send(err));
-                };
-            });
-        } else {
-            fs.writeFile('./logs/deletedmessages/' + (`${messageDelete.author.id}`) + '/' + (date) + '/' + (timeLogs) + ' (' + ((totalFiles)+1) + ')' + '.txt', 'Message from ' + (`${messageDelete.author}`) + '(' + (`${messageDelete.author.username}`) + ') has been deleted.\n\nMessage Content:\n' + (`${messageDelete.content}`) + '\n\nChannel:\n#' + (`${messageDelete.channel.name}`) + '\n\nDate & Time:\n' + (date) + ' @ ' + (time), function (err) {
-                if (err) {
-                    return console.log(err)
-                    .then(bot.channels.get(logCH).send(err));
-                };
-            });
-        }
-    });
+        body = `${messageDelete.content}`
+
+    }
+
+    const embed = new Discord.RichEmbed()
+            .setColor(darkblue)
+            .setAuthor(`${messageDelete.author.username}`, `${messageDelete.author.avatarURL}`)
+            .setTitle("Message Deleted:")
+            .setDescription(`Message sent by ${messageDelete.author} has been deleted.`)
+            .addField("Message content: ", (body))
+            .addField("Channel:", `#${messageDelete.channel.name}`)
+            .addField("Date & Time:", `${date} @ ${time}`)
+    bot.channels.get(logCH).send(embed)
 }
