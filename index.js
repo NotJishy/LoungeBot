29710c6ce06c7317a2916e776c156a35c18effd7
NotJishy <jishydoods@gmail.com>
2020-04-13 21:22:44 -0400

    Changed to version 1.6

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const ms = require("ms");
 // Logs Channel
 const logCH = config.logsChannel;
 
-var version = '1.5';
+var version = '1.6';
 
 // Reports Channel ID
 const reportCH = config.reportsChannel;
