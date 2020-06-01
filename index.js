8bce95339ab153b16eca81d5246b41f10bbb6806
NotJishy <jishydoods@gmail.com>
2020-06-01 15:05:14 -0400

    Edited 'tea' autoresponse

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ bot.on('message', async msg => {
         msg.channel.send('you need some phone juice')
     }
 
-    if ((msgLower === 'tea' || msgLower.includes(' tea') || msgLower.includes('tea ') || msgLower.includes('🍵')) && !msg.author.bot) {
+    if ((msgLower === 'tea' || msgLower.includes('tea ') || msgLower.includes('🍵')) && !msg.author.bot) {
         msg.react(bot.emojis.get('694259745128710394'))
         msg.channel.send('*thats the tea sis*')
     }
